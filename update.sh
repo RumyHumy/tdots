78a4442b4a8dee82ce2fb0c765194500232c1e37
RumyHumy <ramil345655@gmail.com>
2024-08-19 19:39:35 +0300

    Mon Aug 19 19:39:35 MSK 2024

--- update.sh
+++ update.sh
@@ -10,10 +10,12 @@ rm ~/.zshrc
 ln -s ~/tdots/conf/.zshrc ~/.zshrc
 
 #cp -r ~/tdots/conf/nvim/* ~/.config/nvim
+rm ~/.config/init.vim
 mkdir -p ~/.config/nvim
-ln -s ~/tdots/conf/nvim/init.vim
+ln -s ~/tdots/conf/nvim/init.vim ~/.config/init.vim
 
 #cp -r ~/tdots/conf/lf/lfrc ~/.config/lf
+rm ~/.config/lf/lfrc
 mkdir -p ~/.config/lf
 ln -s ~/tdots/conf/lf/lfrc ~/.config/lf/lfrc
 
