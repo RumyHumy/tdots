6f6059fc93d3534fa5963c725ffb34330747db4b
RumyHumy <ramil345655@gmail.com>
2024-08-24 01:15:56 +0300

    Sat Aug 24 01:15:56 MSK 2024

--- scr/update.sh
+++ scr/update.sh
@@ -5,19 +5,16 @@ if [[ $(echo $SHELL | awk -F/ '{print $NF}') != "zsh" ]]; then
 	chsh -s /usr/bin/zsh
 fi
 
-#cp ~/tdots/conf/.zshrc ~
 rm ~/.zshrc
-ln -s ~/tdots/conf/.zshrc ~/.zshrc
+ln -s ~/tdots/conf/zshrc ~/.zshrc
 
-#cp -r ~/tdots/conf/nvim/* ~/.config/nvim
 rm ~/.config/nvim/init.vim
 mkdir -p ~/.config/nvim
 ln -s ~/tdots/conf/nvim/init.vim ~/.config/nvim/init.vim
 
-#cp -r ~/tdots/conf/lf/lfrc ~/.config/lf
 rm ~/.config/lf/lfrc
 mkdir -p ~/.config/lf
 ln -s ~/tdots/conf/lf/lfrc ~/.config/lf/lfrc
 
 cd ~/tdots
-~/tdots/sync.sh
+~/tdots/scr/sync.sh
