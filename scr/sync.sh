b978681f88bf3af44a904d661c8ae34e6b6b3f2d
RumyHumy <ramil345655@gmail.com>
2024-08-24 02:07:44 +0300

    Sat Aug 24 02:07:44 MSK 2024

--- scr/sync.sh
+++ scr/sync.sh
@@ -4,7 +4,7 @@ cd ~/tdots
 
 git status | grep -q "nothing to commit" ; echo $?
 
-if [ ! git status | grep -q "nothing to commit"]; then
+if [$@ -ne 0]; then
     echo "[TDOTS] Nothing to sync!"
 	exit 0
 fi
